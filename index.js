6f9ebe8b25b6ee01cda0895352f3b596dd74d9e4
Reid Evans <reidev275@gmail.com>
2017-09-08 09:02:19 -0400

    remove unused import

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-#!/usr/bin/env node
-
 const { compose } = require('ramda')
-const { Future, Either } = require('ramda-fantasy')
 const { loadApp } = require('./src/bootstrap')
 const { OK } = require('./src/writers')
 const { GET, POST, choose, path, request, then } = require('./src/filters')
